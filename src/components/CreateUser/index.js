75de2485d9472bbe0f3dd6e00eeeffd7eb4db716
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-12 01:53:18 +0300

    created create user component

--- src/components/CreateUser/index.js
+++ src/components/CreateUser/index.js
@@ -1,13 +1,30 @@
 import React, { useState } from 'react'
 
 const CreateUser = () => {
-	const [state, setState] = useState({
-		username: '',
-	})
+	const [username, setUsername] = useState('')
+
+	const changeUsername = (e) => {
+		setUsername(e.target.value)
+	}
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+		console.log(username)
+		setUsername('')
+	}
 
 	return (
 		<div>
-			<h1>You are on Create User component</h1>
+			<h3>Create New User</h3>
+			<form onSubmit={onSubmit}>
+				<div className='form-group'>
+					<label>Username: </label>
+					<input type='text' required value={username} onChange={changeUsername} />
+				</div>
+				<div className='form-group'>
+					<input type='submit' value='Create User' className='btn btn-primary' />
+				</div>
+			</form>
 		</div>
 	)
 }
