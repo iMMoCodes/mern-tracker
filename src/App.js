24663a83eef05f6c3c9db8a48f4ffce3704130d0
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-12 00:43:57 +0300

    created navbar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Navbar from './components/Navbar'
+// import ExercisesList from './components/ExercisesList'
+// import EditExercise from './components/EditExercise'
+// import CreateExercise from './components/CreateExercise'
+// import CreateUser from './components/CreateUser'
+
 function App() {
-	return <div className='container'>Hello</div>
+	return (
+		<Router>
+			<Navbar />
+			{/* <br />
+			<Route path='/' exact component={ExercisesList} />
+			<Route path='/edit/:id' exact component={EditExercise} />
+			<Route path='/create' exact component={CreateExercise} />
+			<Route path='/user' exact component={CreateUser} /> */}
+		</Router>
+	)
 }
 
 export default App
